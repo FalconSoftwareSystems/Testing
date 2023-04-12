d74867ced4ac6beb60e206aaeeedd044cb0f6279
Kartavya Gaur <68074579+kartavyagaur@users.noreply.github.com>
2023-04-12 09:12:11 -0400

    name print from text file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("hi aaron");
+    public static void main(String[] args) throws FileNotFoundException  {
+
+  
+      
+    try {
+      File file1 = new File("src/resources/names.txt");
+      Scanner scan1 = new Scanner(file1);
+      while (scan1.hasNextLine()) {
+        String data = scan1.nextLine();
+        System.out.println(data);
+      }
+     scan1.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
+      
+  }
 }
